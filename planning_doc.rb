3cc89ff1e95aa2ca908542643a7e6254170377e6
Daniel <danielseniv@outlook.com>
2023-05-07 19:29:14 -0400

    Add original plan document to tackle this assignment

---
+++ planning_doc.rb
@@ -0,0 +1,14 @@
+3 classes - Questions, Players and Game
+--Each can have an instance of something
+--Instantiate player 1, 2 and pass them to game class
+
+
+Need to 
+
+--Create two random numbered variables, that get passed into string interp to create questions
+
+--Grab user input and compare it to the answer to question
+
+--Print confirmatory messages based on right / wrong answer -- further call if game over or not
+
+--Switch between the players and keeping their score
